f8bf0bf10d3915545c7c42ce7b83aa12970a541a
Your Name <you@example.com>
2023-12-02 04:14:02 -0700

    rendered table

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://wxvtnddwjkxjaddrfgif.supabase.co',
+ 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4dnRuZGR3amt4amFkZHJmZ2lmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMTE4NDAwNywiZXhwIjoyMDE2NzYwMDA3fQ.B9Lu5HBR_aPN5i0VQiCdYavrhrkWjaLXxv_Cj34RS1E'
+);
